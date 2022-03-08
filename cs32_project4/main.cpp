04abe591a18000ead426e5c7415a8b92fbeca534
dagny parayao <dagny.parayao@gmail.com>
2022-03-07 23:48:17 -0800

    Initial Commit

---
+++ cs32_project4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs32_project4
+//
+//  Created by dagny parayao on 3/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
