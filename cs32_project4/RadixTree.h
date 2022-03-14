86fb41ff8e090a415fc8f3df7fcddbb0a8028d05
dagny parayao <dagny.parayao@gmail.com>
2022-03-14 01:55:35 -0700

    finished radixtree

--- cs32_project4/RadixTree.h
+++ cs32_project4/RadixTree.h
@@ -10,24 +10,89 @@
 
 using namespace std;
 
+struct RadixTreeNode;
+
 template <typename ValueType>
 class RadixTree {
 public:
-    RadixTree() {};
-    ~RadixTree() {};
+    RadixTree()
+    {
+        root = new RadixTreeNode();
+    };
+    ~RadixTree()
+    {
+        //delete root node
+        delete root;
+        
+        //use recursion - make helper function to delete nodes
+    };
     void insert(string key, const ValueType& value);
     ValueType* search(string key) const;
 private:
+    struct RadixTreeNode
+    {
+        RadixTreeNode()
+        {
+            word = "";
+        }
+        
+        RadixTreeNode(string n_word, ValueType n_value)
+        {
+        
+            word = n_word;
+            endOfWord = true;
+            value = n_value;
+        };
+        
+        string word;
+        bool endOfWord;
+        ValueType value;
+        
+        RadixTreeNode* parent = nullptr;
+        RadixTreeNode* edges[128] = {};
+    };
+    
     unordered_map <string, ValueType> fakeTree;
     int numPairs = 0;
-    //real has function to help search in general - that will return key to be used by insert and search
+    RadixTreeNode* root = nullptr;
     
+    //helper functions
+    bool lookup(RadixTreeNode*& node, string& key) const;
 };
 
 template <typename ValueType>
 void RadixTree<ValueType>::insert(string key, const ValueType& value)
 {
-    fakeTree[key] = value;
+    RadixTreeNode* searcher = root;
+    string keyCopy = key;
+    
+    int letter = key[0] - 'a';
+    
+    if((*searcher).edges[letter] == nullptr)
+    {
+        cout << "inserting" << endl;
+        RadixTreeNode* newNode = new RadixTreeNode(key, value);
+        (*searcher).edges[letter] = newNode;
+    }
+    else
+    {
+        searcher = (*searcher).edges[letter];
+        
+        //if returns true, the key is in the tree
+        if(lookup(searcher, keyCopy))
+        {
+            cout << "not inserting" << endl;
+            (*searcher).value = value;
+        }
+        else
+        {
+            cout << "inserting" << endl;
+            //if it returns false, the whole key is not in the tree
+            letter = keyCopy[0] - 'a';
+            RadixTreeNode* newNode = new RadixTreeNode(keyCopy, value);
+            (*searcher).edges[letter] = newNode;
+        }
+    }
 }
 
 /*
@@ -36,19 +101,94 @@ void RadixTree<ValueType>::insert(string key, const ValueType& value)
 template <typename ValueType>
 ValueType* RadixTree<ValueType>::search(string key) const
 {
-    typename unordered_map<string, ValueType>::const_iterator iter;
+    RadixTreeNode* searcher = root;
+    string keyCopy = key;
     
-    iter = fakeTree.find(key);
+    int letter = key[0] - 'a';
     
-    if(iter == fakeTree.end())
+    if((*searcher).edges[letter] == nullptr)
     {
+        cout << "not found" << endl;
         return nullptr;
     }
     else
     {
-        return const_cast<ValueType*>(&(iter->second));
+        searcher = (*searcher).edges[letter];
+        
+        //if returns true, the key is in the tree
+        if(lookup(searcher, keyCopy))
+        {
+            cout << "found" << endl;
+            ValueType* hold = &((*searcher).value);
+            return hold;
+        }
+        else
+        {
+            cout << "not found" << endl;
+            //if returns false, the key is not in the tree
+            return nullptr;
+        }
+    }
+}
+
+//returns if it reached the end of the key or not
+//if true, means there is the key in the tree
+//if false, means there is no key in the tree
+//node either holds the node with the key, or a prefix of the word
+template <typename ValueType>
+bool RadixTree<ValueType>::lookup(RadixTreeNode*& node, string& key) const
+{
+    //note: since trie, trying to find node whose prefix matches the key's prefix
+    RadixTreeNode* current = node;
+    int letter = key[0] - 'a';
+    string prefix = "";
+    string curWord = "";
+    int endpoint = 0;
+    
+    while(endpoint < key.length() || endpoint < (*current).word.length())
+    {
+        if(key[endpoint] != (*current).word[endpoint])
+        {
+            //reached end of matching prefix
+            break;
+        }
+        else
+        {
+            prefix += key[endpoint];
+            curWord += (*current).word[endpoint];
+            endpoint++;
+        }
     }
     
+    if((endpoint == key.length() || endpoint == (*current).word.length()) && prefix == curWord)
+    {
+        //if reached end of one of the words and they are equal/same word
+        cout << prefix << " this way" << endl;
+        return true;
+    }
+    else if(prefix == curWord)
+    {
+        //check edges
+        //if the pointer at the int of the array is nullptr, return the current radixtreenode
+        //if not, go to the radixtreenode that is held in the array
+        letter = key[endpoint] - 'a';
+        if((*current).edges[letter] == nullptr)
+        {
+            //means have same prefix, and there is an empty spot for the rest of the word
+            return false;
+        }
+        else
+        {
+            key = key.substr(endpoint);
+            current = (*current).edges[letter];
+            return lookup(current, key);
+        }
+    }
+    else
+    {
+        //reached the end of one of the words and they are not same word
+        return false;
+    }
 }
 
 #endif /* RadixTree_h */
--- cs32_project4/utility.cpp
+++ cs32_project4/utility.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
 #include "provided.h"
+#include "RadixTree.h"
 
 using namespace std;
 
 void testPersonProfile()
 {
     
+}
+
+void testRadixTree()
+{
+    RadixTree<int>* test = new RadixTree<int>;
+    (*test).insert("help",400);
+    (*test).insert("apple",3);
+    (*test).insert("helper",500);
+    (*test).insert("helping",570);
+    int* testint = (*test).search("helping");
+    cout << *testint << endl;
 }
